b7ab3c9323a8a4cb686f1358b4544c5b32562e18
pxqing <panxiuqing@gmail.com>
2019-01-22 16:44:59 +0800

    update

---
+++ backup/node-ext-builder/README.md
@@ -0,0 +1,2 @@
+# node-ext-builder
+docker image for node ext build env
---
+++ backup/push-prod.sh
@@ -0,0 +1,2 @@
+docker tag ars-node:latest registry.cn-hangzhou.aliyuncs.com/yiyou/ars-node:latest
+docker push registry.cn-hangzhou.aliyuncs.com/yiyou/ars-node:latest
